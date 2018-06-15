361f404c13cb75475a2371c20a8ddb3174b96421
Parminder Singh <parminder.s@kbihm.com>
2018-06-15 15:04:35 +0530

    message1

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express =require('express');
 var app = express();
 var port=process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
-
 
 app.get('/',function(req,res){
 console.log('hello from server');
