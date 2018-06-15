42e3b26fd1a9a512bcc6f452d8bf88a388bf3be2
Parminder Singh <parminder.s@kbihm.com>
2018-06-15 18:21:26 +0530

    message1

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ var express =require('express');
 var app = express();
 var port=process.env.PORT || 3000;
 
-
 app.get('/',function(req,res){
-console.log('hello from server');
- res.end("hello server ");
-
-});
-
-app.listen(port);
-console.log('Server Listening at port'+port);
-
+  console.log('hello from server');
+   res.end("hello user1");
+  
+  
+  });
+  
+  
+  app.listen(port);
+  console.log('Server Listening at port'+port);
+  
 
 //  //start mysql connection
 //  var connection = mysql.createConnection({
