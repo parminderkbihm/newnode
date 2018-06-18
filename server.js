664e3a924e6affb591dc494a79306b84e941e6b1
parminderkbihm <parminderkbihm>
2018-06-18 18:36:07 +0530

    message1

--- server.js
+++ server.js
@@ -1,6 +1,57 @@
 var express =require('express');
 var app = express();
 var port=process.env.PORT || 3000;
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+
+var config = 
+   {
+     userName: 'kbihmcheckdb', // update me
+     password: '%lUy@Gl(&*2wlP', // update me
+     server: 'kbihm.com', // update me
+     options: 
+        {
+           database: 'kbihmcheckdb' //update me
+           , encrypt: true
+        }
+   }
+   var connection = new Connection(config);
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err) 
+   {
+     if (err) 
+       {
+          console.log(err)
+       }
+    else
+       {
+           queryDatabase()
+       }
+   }
+ );
+
+
+ function queryDatabase()
+ { console.log('Reading rows from the Table...');
+
+     // Read all rows from table
+   request = new Request(
+        "select * from Attendence",
+           function(err, rowCount, rows) 
+              {
+                  console.log(rowCount + ' row(s) returned');
+                  process.exit();
+              }
+          );
+
+   request.on('row', function(columns) {
+      columns.forEach(function(column) {
+          console.log("%s\t%s", column.metadata.colName, column.value);
+       });
+           });
+   connection.execSql(request);
+ }
+
 
 
 app.get('/',function(req,res){
