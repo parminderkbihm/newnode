5bdbd86c7809b2fe2bc786ab6e9d93071d7a4a47
parminderkbihm <parminderkbihm>
2018-06-19 12:13:42 +0530

    message1

--- server.js
+++ server.js
@@ -1,64 +1,42 @@
 var express =require('express');
 var app = express();
 var port=process.env.PORT || 3000;
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
+var mysql=require('mysql')
+var bodyParser = require('body-parser');
 
-var config = 
-   {
-     userName: 'kbihmcheckdb', // update me
+
+var connection = mysql.createConnection
+   ({
+    host :   'kbihm.com',
+     user: 'kbihmcheckdb', // update me
      password: '%lUy@Gl(&*2wlP', // update me
-     server: 'kbihm.com', // update me
-     options: 
-        {
-           database: 'kbihmcheckdb' //update me
-           , encrypt: true
-        }
-   }
-   var connection = new Connection(config);
-// Attempt to connect and execute queries if connection goes through
-connection.on('connect', function(err) 
-   {
-     if (err) 
-       {
-          console.log(err)
-       }
-    else
-       {
-           queryDatabase()
-       }
-   }
- );
+     server: 'kbihm.com', 
+    database: 'kbihmcheckdb' //update me
+         
+   });
+  
+connection.connect(function (err) {
+    if (err) throw err
+    console.log('You are now connected with mysql database...')
+  })
 
 
- function queryDatabase()
- { console.log('Reading rows from the Table...');
 
-     // Read all rows from table
-   request = new Request(
-        "select * from Attendence",
-           function(err, rowCount, rows) 
-              {
-                  console.log(rowCount + ' row(s) returned');
-                  process.exit();
-              }
-          );
+   app.use(bodyParser.json());       // to support JSON-encoded bodies
+   app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+     extended: true
+   }));
 
-   request.on('row', function(columns) {
-      columns.forEach(function(column) {
-          console.log("%s\t%s", column.metadata.colName, column.value);
-       });
-           });
-   connection.execSql(request);
- }
-
+   app.use(bodyParser.json());       // to support JSON-encoded bodies
+   app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+     extended: true
+   }));
 
 
 app.get('/',function(req,res){
-console.log('hello from server');
- res.end("hello server ");
-
+    connection.query('select * from Attendence',function(error, results, fields){
+    res.end('j')
+    });
 });
-
-app.listen(port);
+app.listen(port)
 console.log('Server Listening at port'+port);
