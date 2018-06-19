ae56c5a36174c4bbe43da8f11ed88b2483009f4d
parminderkbihm <parminderkbihm>
2018-06-19 12:41:57 +0530

    message1

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
 var express =require('express');
 var app = express();
 var port=process.env.PORT || 3000;
-var mysql=require('mysql')
-var bodyParser = require('body-parser');
+// var mysql=require('mysql')
+// var bodyParser = require('body-parser');
 
 
-var connection = mysql.createConnection
-   ({
-    host :   'kbihm.com',
-     user: 'kbihmcheckdb', // update me
-     password: '%lUy@Gl(&*2wlP', // update me
-     server: 'kbihm.com', 
-    database: 'kbihmcheckdb' //update me
+// var connection = mysql.createConnection
+//    ({
+//     host :   'kbihm.com',
+//      user: 'kbihmcheckdb', // update me
+//      password: '%lUy@Gl(&*2wlP', // update me
+//      server: 'kbihm.com', 
+//     database: 'kbihmcheckdb' //update me
          
-   });
+//    });
   
-connection.connect(function (err) {
-    if (err) throw err
-    console.log('You are now connected with mysql database...')
-  })
+// connection.connect(function (err) {
+//     if (err) throw err
+//     console.log('You are now connected with mysql database...')
+//   })
 
 
 
-   app.use(bodyParser.json());       // to support JSON-encoded bodies
-   app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-     extended: true
-   }));
+//    app.use(bodyParser.json());       // to support JSON-encoded bodies
+//    app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+//      extended: true
+//    }));
 
 app.get('/',function(req,res){
-    connection.query('select * from Attendence',function(error, results, fields){
-    res.write(JSON.stringify(results))
+    // connection.query('select * from Attendence',function(error, results, fields){
+    // res.write(JSON.stringify(results))
         res.end('hellooooooo');
     
-  });
+//   });
 });
 app.listen(port)
 console.log('Server Listening at port'+port);
