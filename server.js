558a509051c06167ded04e25250e6f3124898cad
parminderkbihm <parminderkbihm>
2018-06-19 12:53:34 +0530

    message1

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ connection.connect(function (err) {
 app.get('/',function(req,res){
     // connection.query('select * from Attendence',function(error, results, fields){
     // res.write(JSON.stringify(results))
-        res.end('hello');
+        res.end('he');
     
 //   });
 });
