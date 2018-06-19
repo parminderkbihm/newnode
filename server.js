d227dc4f879612a4b545c92ce7047fbbe64aa042
parminderkbihm <parminderkbihm>
2018-06-19 12:22:59 +0530

    message1

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
 var express =require('express');
 var app = express();
 var port=process.env.PORT || 3000;
-var mysql=require('mysql')
-var bodyParser = require('body-parser');
 
-
-var connection = mysql.createConnection
-   ({
-    host :   'kbihm.com',
-     user: 'kbihmcheckdb', // update me
-     password: '%lUy@Gl(&*2wlP', // update me
-     server: 'kbihm.com', 
-    database: 'kbihmcheckdb' //update me
-         
-   });
-  
-connection.connect(function (err) {
-    if (err) throw err
-    console.log('You are now connected with mysql database...')
-  })
-
-
-
-   app.use(bodyParser.json());       // to support JSON-encoded bodies
-   app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-     extended: true
-   }));
 
 
 
 app.get('/',function(req,res){
-    connection.query('select * from Attendence',function(error, results, fields){
+    // connection.query('select * from Attendence',function(error, results, fields){
     res.end(JSON.stringify(results))
-    });
+    // });
 });
 app.listen(port)
 console.log('Server Listening at port'+port);
+// var mysql=require('mysql')
+// var bodyParser = require('body-parser');
+
+
+// var connection = mysql.createConnection
+//    ({
+//     host :   'kbihm.com',
+//      user: 'kbihmcheckdb', // update me
+//      password: '%lUy@Gl(&*2wlP', // update me
+//      server: 'kbihm.com', 
+//     database: 'kbihmcheckdb' //update me
+         
+//    });
+  
+// connection.connect(function (err) {
+//     if (err) throw err
+//     console.log('You are now connected with mysql database...')
+//   })
+
+
+
+//    app.use(bodyParser.json());       // to support JSON-encoded bodies
+//    app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+//      extended: true
+//    }));
+
+
