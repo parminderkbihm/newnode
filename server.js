e80d426138fd61abb0bcdfb12948f618c1115027
parminderkbihm <parminderkbihm>
2018-06-19 12:59:56 +0530

    message1

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection
    });
   
 connection.connect(function (err) {
-    if (err) throw err;
+    if (err) console.log('error occured')
     console.log('You are now connected with mysql database...')
   })
 
